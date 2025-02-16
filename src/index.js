bd8ccda4093d69a8f23f598591178c1c03424dca
Abhay123450 <95590546+Abhay123450@users.noreply.github.com>
2025-02-16 13:19:51 +0530

    cors allow all origin

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDB = require('./config/connectDB');
 dotenv.config();
@@ -17,6 +18,8 @@ const { handleErrors } = require('./utils/handleErrors');
     const PORT = process.env.PORT || 3000;
 
     const app = express();
+
+    app.use(cors());
 
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
